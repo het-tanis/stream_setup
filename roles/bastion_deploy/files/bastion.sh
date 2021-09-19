b628315d8000e7da69de5e388b6750afba369b8d
scott.champine@gmail.com <scott.champine@gmail.com>
2021-09-19 14:13:35 -0700

    Added in reporting functionality

--- roles/bastion_deploy/files/bastion.sh
+++ roles/bastion_deploy/files/bastion.sh
@@ -14,8 +14,7 @@ case $input in
 1 )
   echo "Loading a joke"
   #curl command against an api for jokes
-#  /bin/curl -k https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religi
-ous,political,racist,sexist,explicit&format=txt
+#  /bin/curl -k https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religous,political,racist,sexist,explicit&format=txt
   ;;
 2 )
   echo "You are being sent to the streamers server"
@@ -28,8 +27,7 @@ ous,political,racist,sexist,explicit&format=txt
 * )
   echo "You have not entered a valid input"
 #  echo "You get a joke and you get to leave"
-#  /bin/curl -k https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religi
-ous,political,racist,sexist,explicit&format=txt
+#  /bin/curl -k https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt
   exit 0;
   ;;
 
